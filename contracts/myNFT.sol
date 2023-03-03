c84a7c6b9351fea9bb1bbcb2b2d8a3775ddeceb1
Sidharth P <sidharthpunathil714@gmail.com>
2023-03-03 15:49:18 +0530

    ERC-712

--- contracts/myNFT.sol
+++ contracts/myNFT.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -6,33 +7,36 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract myNFT is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
-    mapping(address => uint8) userToTokenAmount;
+    string private _baseTokenURI;
 
-    constructor() ERC721("sid's NFT", "XID") {
-        _tokenIdCounter.increment();
+    constructor(string memory baseTokenURI) ERC721("sid's NFT", "XID") {
+        _baseTokenURI = baseTokenURI;
     }
 
     function safeMint() public {
-        require(userToTokenAmount[msg.sender] < 1, "user already has one NFT");
+        require(
+            !_exists(_tokenIdCounter.current()),
+            "user already has one NFT"
+        );
 
         uint256 tokenId = _tokenIdCounter.current();
         _safeMint(msg.sender, tokenId);
         _tokenIdCounter.increment();
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "IPFS";
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
     }
 
-    function tokenURL(uint256 tokenId)
+    function tokenURI(uint256 tokenId)
         public
         view
         override
         returns (string memory)
     {
-        string memory _tokenURL = string(
+        string memory _tokenURI = string(
             abi.encodePacked(_baseURI(), "/", tokenId, ".json")
         );
-        return _tokenURL;
+        return _tokenURI;
     }
 }
